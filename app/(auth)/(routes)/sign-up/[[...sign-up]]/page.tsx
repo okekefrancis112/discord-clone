fa9d478ba5f5408f0f9af4925a2c993ff15a30b2
okekefrancis112 <okeke98@gmail.com>
2025-07-24 19:20:03 +0100

    Profile and server setup

--- app/(auth)/(routes)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/(routes)/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,34 @@
-import { SignUp } from '@clerk/nextjs'
+// import { SignUp } from '@clerk/nextjs'
 
-export default function Page() {
-  return <SignUp />
-}
+// export default function Page() {
+//   return <SignUp />
+// }
+
+"use client";
+
+import { useCurrentTheme } from "@/hooks/use-current-theme";
+import { SignUp } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
+
+const Page = () => {
+  const currentTheme = useCurrentTheme();
+
+  return (
+    <div className="flex flex-col max-w-3xl mx-auto w-full">
+        <section className="space-y-6 pt-[16vh] 2xl:pt-48">
+            <div className="flex flex-col items-center">
+                <SignUp
+                    appearance={{
+                        baseTheme: currentTheme === "dark" ? dark : undefined,
+                        elements: {
+                            cardBox: "border! shadow-none! rounded-lg!",
+                        },
+                    }}
+                />
+            </div>
+        </section>
+    </div>
+  );
+};
+
+export default Page;
