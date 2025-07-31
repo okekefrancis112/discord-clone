1a91743e2e6d5a7d3448564498e3c8a58cd22f84
Francis Chukwudalu Okeke <okeke98@gmail.com>
2025-07-31 04:42:27 +0100

    Update app/api/servers/[serverId]/invite-code/route.ts
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- app/api/servers/[serverId]/invite-code/route.ts
+++ app/api/servers/[serverId]/invite-code/route.ts
@@ -30,7 +30,7 @@ export async function PATCH(
 
         return NextResponse.json(server);
     } catch (error) {
-        console.log("[SERVER_ID", error);
+        console.log("[SERVER_ID]", error);
         return new NextResponse("Internal Error", { status: 500 });
     }
 }
