eaeef6b5d542c829d1f3b3a51daffc01578788de
okekefrancis112 <okeke98@gmail.com>
2025-08-05 13:05:42 +0100

    channel features updated, also chat and members area added.

--- app/(main)/(routes)/servers/[serverId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/page.tsx
@@ -1,9 +1,54 @@
-const ServerPage
- = () => {
-    return (
-        <div>Server ID Page</div>
-    );
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { RedirectToSignIn } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+interface ServerIdPageProps {
+    params: {
+        serverId: string;
+    }
+};
+
+const ServerIdPage = async ({
+    params
+}: ServerIdPageProps) => {
+    const profile = await currentProfile();
+
+    // Await params before using it
+    const { serverId } = await params;
+
+    if(!profile) {
+        return RedirectToSignIn({});
+    }
+
+    const server = await db.server.findUnique({
+        where: {
+            id: serverId,
+            members: {
+                some: {
+                    profileId: profile.id,
+                },
+            },
+        },
+        include: {
+            channels: {
+                where: {
+                    name: "general"
+                },
+                orderBy: {
+                    createdAt: "asc"
+                },
+            },
+        },
+    });
+
+    const initialChannel = server?.channels[0];
+
+    if (initialChannel?.name !== "general") {
+        return null;
+    }
+
+    return redirect(`/servers/${serverId}/channels/${initialChannel?.id}`);
 }
 
-export default ServerPage
-;
+export default ServerIdPage;
--- components/server/server-sidebar.tsx
+++ components/server/server-sidebar.tsx
@@ -180,26 +180,6 @@ export const ServerSidebar = async ({
                         </div>
                     </div>
                 )}
-                {!!videoChannels?.length && (
-                    <div className="mb-2">
-                        <ServerSection
-                            sectionType="channels"
-                            channelType={ChannelType.VIDEO}
-                            role={role}
-                            label="Video Channels"
-                        />
-                        <div className="space-y-[2px]">
-                            {videoChannels.map((channel) => (
-                                <ServerChannel
-                                    key={channel.id}
-                                    channel={channel}
-                                    role={role}
-                                    server={server}
-                                />
-                            ))}
-                        </div>
-                    </div>
-                )}
                 {!!members?.length && (
                     <div className="mb-2">
                         <ServerSection
