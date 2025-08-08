24c7433099cfda99ed97a80d13c04ea2d79d22ac
okekefrancis112 <okeke98@gmail.com>
2025-08-08 03:36:17 +0100

    chat features implemented

--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -1,10 +1,74 @@
+// "use client";
+
+// import { UploadDropzone } from "@/lib/uploadthing";
+// import Image from "next/image";
+
+// // import "@uploadthing/react/styles.css";
+// import { X } from "lucide-react";
+
+// interface FileUploadProps {
+//   onChange: (url: string) => void;
+//   value: string;
+//   endpoint: "messageFile" | "serverImage";
+// }
+
+// export const FileUpload = ({
+//   onChange,
+//   value = "",
+//   endpoint
+// }: FileUploadProps) => {
+//   const fileType = value?.split(".").pop();
+
+//   const handleRemove = () => {
+//     onChange(""); // Clear the value
+//   };
+
+//   console.log("value>>>>>>>>>>>>>>>>>>>>>>>>>: ", value)
+//   console.log("fileType>>>>>>>>>>>>>>>>>>>>>>>>>: ", fileType)
+
+//   if (value && fileType !== "pdf") {
+//     return (
+//       <div className="relative h-20 w-20">
+//         <Image
+//           fill
+//           src={value}
+//           alt="Upload"
+//           className="rounded-full"
+//           sizes="80px" // Added sizes prop
+//         />
+//         <button
+//           onClick={handleRemove}
+//           className="bg-rose-500 text-white p-1 rounded-full absolute top-0 right-0 shadow-sm"
+//           type="button"
+//         >
+//           <X className="h-4 w-4" />
+//         </button>
+//       </div>
+//     );
+//   }
+
+//   return (
+//     <UploadDropzone
+//       endpoint={endpoint}
+//       onClientUploadComplete={(res) => {
+//         // onChange(res?.[0]?.url || res?.[0]?.ufsUrl || "");
+//         // onChange(res?.[0]?.ufsUrl);
+//         console.log("res?.[0]?.ufsUrl>>>>>>>>>>>>>>>>>>", res?.[0]?.ufsUrl);
+//       }}
+//       onUploadError={(error: Error) => {
+//         console.error("Upload error:", error);
+//       }}
+//     />
+//   );
+// };
+
 "use client";
 
 import { UploadDropzone } from "@/lib/uploadthing";
 import Image from "next/image";
 
 // import "@uploadthing/react/styles.css";
-import { X } from "lucide-react";
+import { FileIcon, X } from "lucide-react";
 
 interface FileUploadProps {
   onChange: (url: string) => void;
@@ -23,8 +87,7 @@ export const FileUpload = ({
     onChange(""); // Clear the value
   };
 
-  console.log("value>>>>>>>>>>>>>>>>>>>>>>>>>: ", value)
-  console.log("fileType>>>>>>>>>>>>>>>>>>>>>>>>>: ", fileType)
+  console.log("fileType>>>>>>>>>>>>>>>>>>>>>>", fileType)
 
   if (value && fileType !== "pdf") {
     return (
@@ -47,13 +110,35 @@ export const FileUpload = ({
     );
   }
 
+  if (value && fileType === "pdf") {
+    return (
+      <div className="relative flex items-center p-2 mt-2 rounded-md bg-background/10">
+        <FileIcon className="h-10 w-10 fill-indigo-200 stroke-indigo-400" />
+        <a
+          href={value}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="ml-2 text-sm text-indigo-500 dark:text-indigo-400 hover:underline"
+        >
+          {value}
+        </a>
+        <button
+          onClick={() => onChange("")}
+          className="bg-rose-500 text-white p-1 rounded-full absolute -top-2 -right-2 shadow-sm"
+          type="button"
+        >
+          <X className="h-4 w-4" />
+        </button>
+      </div>
+    )
+  }
+
   return (
     <UploadDropzone
       endpoint={endpoint}
       onClientUploadComplete={(res) => {
         // onChange(res?.[0]?.url || res?.[0]?.ufsUrl || "");
-        // onChange(res?.[0]?.ufsUrl);
-        console.log("res?.[0]?.ufsUrl>>>>>>>>>>>>>>>>>>", res?.[0]?.ufsUrl);
+        onChange(res?.[0]?.ufsUrl);
       }}
       onUploadError={(error: Error) => {
         console.error("Upload error:", error);
